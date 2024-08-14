adb5b201b48a60ea37487c7800efde8c7723c73c
T.Putra  Agastiya <ageztya.putra@gmail.com>
2024-08-14 16:41:28 +0700

    Update app.go

--- App/app.go
+++ App/app.go
@@ -1,10 +1,21 @@
 package App
 
 import (
+	"os"
+	"os/signal"
 	"scheduler-backup-postgresql/Config"
 	scheduler "scheduler-backup-postgresql/Scheduler"
+	"syscall"
 )
 
+func ServiceInit() {
+	environment := Config.GetEnvironment()
+	scheduler.SchedulerStart(environment)
+}
+
 func AppInitialization() {
-	scheduler.SchedulerStart(Config.GetEnvironment())
+	ServiceInit()
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
 }
